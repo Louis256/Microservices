05a4ba5847a5f333a13dc3934efc93ff7c7f9471
Luyin Huang <luyinhuang.nathaniel@gmail.com>
2019-08-20 11:00:07 -0400

    Add existing file

---
+++ spring-boot-exam/src/main/java/com/boot/service/QuestionService.java
@@ -0,0 +1,12 @@
+package com.boot.service;
+
+import java.util.Map;
+
+import com.boot.domain.Question;
+
+public interface QuestionService {
+	Question selectQuestionById(Integer questionId);
+
+	Question selectImageById(Integer questionId); 
+
+}
