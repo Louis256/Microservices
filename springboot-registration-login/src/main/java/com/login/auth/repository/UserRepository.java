e4dc6d0c28d04ab65eea76539bf3e27d0a09cbfb
Luyin Huang <luyinhuang.nathaniel@gmail.com>
2019-08-20 09:10:42 -0400

    Add existing file

---
+++ springboot-registration-login/src/main/java/com/login/auth/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.login.auth.repository;
+
+import com.login.auth.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
---
+++ springboot-registration-login/src/main/java/com/login/auth/service/SecurityService.java
@@ -0,0 +1,7 @@
+package com.login.auth.service;
+
+public interface SecurityService {
+    String findLoggedInUsername();
+
+    void autoLogin(String username, String password);
+}
